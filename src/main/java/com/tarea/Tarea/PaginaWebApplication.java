4e06b535189f7fe13a2652a7fff05b5118455e58
SaulMerloS <smerlo2014@gmail.com>
2023-10-04 21:13:14 -0700

    Inicializacion del proyecto No se subio solamente el index, se avanzo en la pagina web No se hizo por pasos, asi que solo quedan por desarrollar 2 paginas

---
+++ src/main/java/com/tarea/Tarea/PaginaWebApplication.java
@@ -0,0 +1,13 @@
+package com.tarea.Tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea/Tarea/PaginaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea.Tarea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
